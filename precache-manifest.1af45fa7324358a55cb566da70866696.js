80c970a7ef4c0940ec55ffe0e18368ce244a7ecf
haithem <haithem196@gmail.com>
2019-11-16 19:37:15 +0100

    Updates

---
+++ precache-manifest.1af45fa7324358a55cb566da70866696.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8450511ab81b97393d5ce672799676fd",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6e64566c3eee0a690031",
+    "url": "/monsters-rolodex/static/css/main.95d77623.chunk.css"
+  },
+  {
+    "revision": "a20f2964f1e7103d046d",
+    "url": "/monsters-rolodex/static/js/2.3860c490.chunk.js"
+  },
+  {
+    "revision": "6e64566c3eee0a690031",
+    "url": "/monsters-rolodex/static/js/main.1785d560.chunk.js"
+  },
+  {
+    "revision": "939568b41133ead5a647",
+    "url": "/monsters-rolodex/static/js/runtime-main.183c07c7.js"
+  }
+]);
